c9a6d379bafc7e9adaae91e69cfe837265803619
rahulhingve <rahulpawar2001.rp@gmail.com>
2024-05-14 10:32:26 +0530

    Capitalized the user First Name in Appbar component

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -1,4 +1,10 @@
-export function Appbar() {
+export function Appbar({name}) {
+
+    function capitalize(s){
+        return s ? s[0].toUpperCase() + s.slice(1) : '';
+    }
+
+
     return <div>
         <div className=" flex justify-between shadow p-4">
             <div className="flex flex-col font-bold text-xl">
@@ -9,11 +15,11 @@ export function Appbar() {
                     Hello,
                 </div>
                 <div className="pr-2 font-medium" >
-                    User
+                    {capitalize(name)}
                 </div>
                 <div className="bg-slate-200 rounded-full h-12 w-12 mt-1 mr-2 flex justify-center">
                     <div className="flex flex-col justify-center h-full text-xl">
-                        U
+                        {capitalize([name ? name[0] : ''])}
                     </div>
 
                 </div>
