6437f2d7fb510e676d8adaad63a023fec4930329
rahulhingve <rahulpawar2001.rp@gmail.com>
2024-05-14 08:36:43 +0530

    Added AuthMiddleWare In User only User will get data now

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -142,7 +142,7 @@ router.put("/", authMiddleware, async (req, res) => {
 
 })
 
-router.get("/bulk",async (req, res) => {
+router.get("/bulk",authMiddleware,async (req, res) => {
     const filter = req.query.filter || "";
 
 
